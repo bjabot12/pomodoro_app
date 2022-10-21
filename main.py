410be289770bba33d7de4b5996aed075ec546124
bjabot12 <bjarte1997@hotmail.com>
2022-10-21 20:56:48 +0200

    first app

--- main.py
+++ main.py
@@ -3,18 +3,37 @@ from PyQt5.QtCore import *
 from PyQt5.QtGui import *
 from PyQt5.QtWidgets import *
 
+class MainWindow(QMainWindow):
+   count = 0
 
-def window():
+   def __init__(self, parent = None):
+      super(MainWindow, self).__init__(parent)
+      self.mdi = QMdiArea()
+      self.setCentralWidget(self.mdi)
+      bar = self.menuBar()
+
+      file = bar.addMenu("Settings")
+      file.addAction("New")
+      file.triggered[QAction].connect(self.windowaction)
+      self.setWindowTitle("MDI demo")
+
+   def windowaction(self, q):
+      print ("triggered")
+   
+      if q.text() == "New":
+         MainWindow.count = MainWindow.count+1
+         sub = QMdiSubWindow()
+         sub.setWidget(QTextEdit())
+         sub.setWindowTitle("subwindow"+str(MainWindow.count))
+         self.mdi.addSubWindow(sub)
+         sub.show()
+
+
+def main():
    app = QApplication(sys.argv)
-   w = QWidget()
-   b = QLabel(w)
-   b.setText("Hello World!")
-   w.setGeometry(100,100,200,50)
-   b.move(50,20)
-   w.setWindowTitle("PyQt5")
-   w.show()
+   ex = MainWindow()
+   ex.show()
    sys.exit(app.exec_())
 
-
 if __name__ == '__main__':
-   window()
+   main()
